1636c1f8b3f361893742cb77bcb47c3125b2fa03
Sixtey7 <sixtey7@gmail.com>
2017-05-07 12:37:31 -0400

    update agricola to calculate score

--- agricola/controllers/agricola.controller.ts
+++ agricola/controllers/agricola.controller.ts
@@ -15,9 +15,22 @@ export default class AgricolaController extends GenericScoredController<IAgricol
 		super(AgricolaGameResult);
 	};
 
-	//TODO: Fix this
 	public calculateScore(firstPlayer: IAgricolaPlayerResultModel, secondPlayer: IAgricolaPlayerResultModel) : IAgricolaPlayerResultModel {
-		firstPlayer.score = secondPlayer.score;
+		firstPlayer.bonusNum = secondPlayer.bonusNum;
+        firstPlayer.cardNum = secondPlayer.cardNum;
+        firstPlayer.clayNum = secondPlayer.clayNum;
+        firstPlayer.cowNum = secondPlayer.cowNum;
+        firstPlayer.familyNum = secondPlayer.familyNum;
+        firstPlayer.fieldsNum = secondPlayer.fieldsNum;
+        firstPlayer.grainNum = secondPlayer.grainNum;
+        firstPlayer.pastureNum = secondPlayer.pastureNum;
+        firstPlayer.pigNum = secondPlayer.pigNum;
+        firstPlayer.sheepNum = secondPlayer.sheepNum;
+        firstPlayer.stableNum = secondPlayer.stableNum;
+        firstPlayer.stoneNum = secondPlayer.stoneNum;
+        firstPlayer.vegNum = secondPlayer.vegNum;
+        firstPlayer.score = secondPlayer.score;
+
 		return firstPlayer;
 	}
 
