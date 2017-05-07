02f23865622394640a0044cdde108add05670dcc
Sixtey7 <sixtey7@gmail.com>
2017-05-06 21:35:59 -0400

    add missing save function

--- shared/generics/routes/generic.scored.routes.ts
+++ shared/generics/routes/generic.scored.routes.ts
@@ -34,6 +34,32 @@ export default class GenericScoredRoutes<T extends GenericScoredController<V, P>
 				res.status(400).send('gameId is a required parameter!');
 			};
 		});
-	}
+
+		app.post('/' + this.prefix + '/save', (req: express.Request, res: express.Response) => {
+			if (req.query.gameId !== undefined) {
+				let newPlayerArray: P[] = req.body;
+				if (newPlayerArray) {
+					console.log('parsed the following:\n' + JSON.stringify(newPlayerArray));
+
+					this.controller.saveGame(req.query.gameId, newPlayerArray)
+						.then(response => {
+							res.status(200).send(response);
+						})
+						.catch(err => {
+							console.error('Got an error attempting to save a game!\n' + err);
+							res.status(500).send(err);
+						})
+				}
+				else {
+					console.error('body of message was not parsable into player array!');
+					res.status(400).send('Could not parse body into player array');
+				}
+			}
+			else {
+				console.error('No gameId found in request to save game');
+				res.status(400).send('gameId is a required parameter!');
+			}
+		});
+	};
 
 }
